0e59c40f67d04a69b13bbc228b1ddf5961c95e9f
Kristin Saunders <kmsaunders7@gmail.com>
2021-02-15 13:44:38 -0600

    add sql info

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ('St. Louis Style');
+INSERT INTO burgers (burger_name) VALUES ('Cheddar Bacon');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Patty Melt', true);
